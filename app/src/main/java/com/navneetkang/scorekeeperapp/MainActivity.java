c6c965247a60bbf2854244fccf9b227483f15514
Navneet Kang <navneetkang.kang@gmail.com>
2020-06-24 14:36:40 +0530

    Scorekeeper App

---
+++ app/src/main/java/com/navneetkang/scorekeeperapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.navneetkang.scorekeeperapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Navneet ");
+    }
+}
